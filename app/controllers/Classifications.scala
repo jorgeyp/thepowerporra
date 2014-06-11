861f9c5b269c8eaad47060029996ecd859ee741e
jorgefs <jorge.fernandez.suarez@gmail.com>
2014-06-11 23:44:26 +0200

    Disable updates (cont)

--- app/controllers/Classifications.scala
+++ app/controllers/Classifications.scala
@@ -33,13 +33,13 @@ object Classifications extends Controller with Secured {
   }
 
   def bet(idRound: Int, idTeam: Int) = IsAuthenticated { email => implicit request =>
-    User.createClassificationBet(idRound, idTeam, email)
+    // User.createClassificationBet(idRound, idTeam, email)
     // Ok
     Forbidden
   }
 
   def delete(idRound: Int) = IsAuthenticated { email => implicit request =>
-    User.deleteClassificationBets(email, idRound)
+    // User.deleteClassificationBets(email, idRound)
     // Ok
     Forbidden
   }
